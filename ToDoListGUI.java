cd46cbc99dfe80eaafa691d7bdba05ddc3a7d6b4
Helix290 <yuki.mitunari.1600@gmail.com>
2020-04-29 14:25:37 +0900

    設定の枠とダークモードの追加

--- ToDoListGUI.java
+++ ToDoListGUI.java
@@ -9,35 +9,109 @@ public class ToDoListGUI extends Applet {
     Graphics2D g2;
     Applet app;
     Image img_home[];
+    int windowsize_x = 1366, windowsize_y = 768;
+    int page = 0, nightmode = 0;
+    
+    int setting_icon_x, setting_icon_y, setting_icon_length;
+    int night_icon_x, night_icon_y, night_icon_length;
+    
+    
     public void init() {
+        //ファイル読み込み
+        
         app = this;
-        //windows size 変数にして変更できるようにする
-        app.setSize(1366, 768);
+        app.setSize(windowsize_x, windowsize_y);
         g = app.getGraphics();
         g2 = (Graphics2D) g;
+        
+        setting_icon_x = windowsize_x - 50;
+        setting_icon_y = 0;
+        setting_icon_length = 50;
+        night_icon_x = windowsize_x - 110;
+        night_icon_y = 0;
+        night_icon_length = 50;
+        
         //画像読み込み
-        int img_homenumber = 4;
+        int img_homenumber = 7;
         img_home = new Image[img_homenumber];
-        for (int i = 1; i <= img_homenumber; i++) {
-            img_home[i - 1] = app.getImage(app.getCodeBase(), "images/Home" + i + ".png");
+        for (int i = 0; i < img_homenumber; i++) {
+            img_home[i] = app.getImage(app.getCodeBase(), "images/Home" + i + ".png");
         }
         //グラフィクスの読み込みを遅滞なく行わせるため
         Image pr = createImage(500,500);
-        for (int i = 1; i < img_homenumber; i++) {
+        for (int i = 0; i < img_homenumber; i++) {
         Graphics gpr = pr.getGraphics();
         gpr.drawImage(img_home[i], 0, 0, app);
         }
+        
         this.addMouseListener(
                 new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                //クリック時の動作
+                if(page == 0){
+                    if(setting_icon_x <= e.getX() && e.getX() <= windowsize_x && setting_icon_y <= e.getY() && e.getY() <= setting_icon_y + setting_icon_length){
+                        page = 1;
+                        repaint();
+                    }
+                    if(night_icon_x <= e.getX() && e.getX() <= night_icon_x + night_icon_length && night_icon_y <= e.getY() && e.getY() <= night_icon_y + night_icon_length){
+                        if(nightmode == 0) nightmode = 1;
+                        else if(nightmode == 1) nightmode = 0;
+                        repaint();
+                    }
+                }
+                else if(page == 1){
+                    if(setting_icon_x <= e.getX() && e.getX() <= windowsize_x && setting_icon_y <= e.getY() && e.getY() <= setting_icon_y + setting_icon_length){
+                        page = 0;
+                        repaint();
+                    }
+                    if(night_icon_x <= e.getX() && e.getX() <= night_icon_x + night_icon_length && night_icon_y <= e.getY() && e.getY() <= night_icon_y + night_icon_length){
+                        if(nightmode == 0) nightmode = 1;
+                        else if(nightmode == 1) nightmode = 0;
+                        repaint();
+                    }
+                }
             }        
         });
     }
     public void paint(Graphics g) {
-        Font f = new Font("Serif", Font.BOLD, 12);
-        g.setFont(f);
-        g.setColor(Color.BLACK);
-        g.drawString("ホーム画面",50, 50);
+        if(page == 0){
+            g.drawImage(img_home[0], setting_icon_x, setting_icon_y, app);
+                if(nightmode == 0){
+                    g.drawImage(img_home[5], night_icon_x, night_icon_y, app);
+                    app.setBackground(Color.white);
+                    Font f = new Font("Serif", Font.BOLD, 25);
+                    g.setFont(f);
+                    g.setColor(Color.BLACK);
+                    g.drawString("ToDoList",15, 30);
+                }
+                else if(nightmode == 1){
+                    g.drawImage(img_home[6], night_icon_x, night_icon_y, app);
+                    app.setBackground(new Color(50, 50, 50));
+                    Font f = new Font("Serif", Font.BOLD, 25);
+                    g.setFont(f);
+                    g.setColor(Color.WHITE);
+                    g.drawString("ToDoList",15, 30);
+                }
+        }
+        else if (page == 1){
+            g.drawImage(img_home[4], setting_icon_x, setting_icon_y, app);
+            if(nightmode == 0){
+                g.drawImage(img_home[5], night_icon_x, night_icon_y, app);
+                app.setBackground(Color.white);
+                Font f = new Font("Serif", Font.BOLD, 25);
+                g.setFont(f);
+                g.setColor(Color.BLACK);
+                g.drawString("ToDoList - 設定",15, 30);
+            }
+            else if(nightmode == 1){
+                g.drawImage(img_home[6], night_icon_x, night_icon_y, app);
+                app.setBackground(new Color(50, 50, 50));
+                Font f = new Font("Serif", Font.BOLD, 25);
+                g.setFont(f);
+                g.setColor(Color.WHITE);
+                g.drawString("ToDoList - 設定",15, 30);
+            }
+            
+        }
+       
     }
 }
