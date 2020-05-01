5bde8c937b9a1f4c470aa404465a7c37c357534c
Helix290 <yuki.mitunari.1600@gmail.com>
2020-05-01 13:26:30 +0900

    todoの記録Classを使えるようにした。 文字入力 TextFieldの追加 todo追加ボタンの削除(機能削減)

--- ToDoListGUI.java
+++ ToDoListGUI.java
@@ -5,32 +5,38 @@ import java.awt.event.*;
 class todolist
 {
     int view_number;
-    char [] data = new char[100];
+    String data;
 }
 
-public class ToDoListGUI extends Applet {
+public class ToDoListGUI extends Applet implements ActionListener{
 
     Graphics g;
     Graphics2D g2;
     Applet app;
     Image img_home[];
+    Button ok;
+    TextField tf1;
     
     int listnum_max = 100;
     int i;
     int windowsize_x = 1366, windowsize_y = 768;
     int page = 0, nightmode = 0, todo_add = 0;
     int class_number = 0;
+    todolist[] list = new todolist[listnum_max];
     
     int setting_icon_x, setting_icon_y, setting_icon_length;
     int night_icon_x, night_icon_y, night_icon_length;
     
     
     public void init() {
-        todolist[] list = new todolist[listnum_max];
+        
         //各要素ごとにインスタンス化
         for(int i = 0; i < list.length; i++){
             list[i] = new todolist();
         }
+/*        for(i = 0; i < listnum_max; i++){
+            list[i] = new todolist();
+        }*/
         
         //ファイル読み込み
         
@@ -38,6 +44,13 @@ public class ToDoListGUI extends Applet {
         app.setSize(windowsize_x, windowsize_y);
         g = app.getGraphics();
         g2 = (Graphics2D) g;
+        ok = new Button("OK");
+        tf1 = new TextField(30);
+        //setLayout(new GridLayout(3,1,10,10));
+        add(tf1);
+        add(ok);
+        tf1.addActionListener(this);
+        ok.addActionListener(this);
         
         setting_icon_x = windowsize_x - 50;
         setting_icon_y = 0;
@@ -47,7 +60,7 @@ public class ToDoListGUI extends Applet {
         night_icon_length = 50;
         
         //画像読み込み
-        int img_homenumber = 9;
+        int img_homenumber = 7;
         img_home = new Image[img_homenumber];
         for (int i = 0; i < img_homenumber; i++) {
             img_home[i] = app.getImage(app.getCodeBase(), "images/Home" + i + ".png");
@@ -70,11 +83,6 @@ public class ToDoListGUI extends Applet {
                     if(night_icon_x <= e.getX() && e.getX() <= night_icon_x + night_icon_length && night_icon_y <= e.getY() && e.getY() <= night_icon_y + night_icon_length){
                         if(nightmode == 0) nightmode = 1;
                         else if(nightmode == 1) nightmode = 0;
-                        repaint();
-                    }
-                    if(windowsize_x - 170 <= e.getX() && e.getX() <= windowsize_x - 120 && 0 <= e.getY() && e.getY() <= 50){
-                        if(todo_add == 0) todo_add = 1;
-                        else todo_add = 0;
                         repaint();
                     }
                 }
@@ -110,14 +118,13 @@ public class ToDoListGUI extends Applet {
                     g.setColor(Color.WHITE);  
                 }
                 g.drawString("ToDoList",15, 30);
-                if(todo_add == 0){
-                    g.drawImage(img_home[7], windowsize_x - 170, 0, app);
-                    g.drawString("(タスクを追加)", windowsize_x - 350, 38);
-                } else{
-                    g.drawImage(img_home[8], windowsize_x - 170, 0, app);
-                }
                 for(i = 0; i < windowsize_y / 60; i++){
                     g.drawLine(0, night_icon_length + 10 + (i * 60), windowsize_x, night_icon_length + 10 + (i * 60));
+                    if(i < class_number){
+                        g.drawString(list[i].data, 80, night_icon_length + 50 + (i * 60));
+                    } else{
+                        
+                    } 
                 }
                 
         }
@@ -141,5 +148,13 @@ public class ToDoListGUI extends Applet {
             }
             
         } 
+    }
+    public void actionPerformed(ActionEvent e){
+        String str = tf1.getText();
+        list[class_number].view_number = class_number;
+        list[class_number].data = str;
+        class_number +=1;
+        tf1.setText("");
+        repaint();
     }
 }
